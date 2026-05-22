06f8a5b0adf9811fe35e6df8280426ce68d2c38a
harylax <haryandr23@gmail.com>
2026-05-22 15:01:09 +0300

    final mod06

--- mod06/alchemy/grimoire/__init__.py
+++ mod06/alchemy/grimoire/__init__.py
@@ -1,5 +1,8 @@
 from .light_spellbook import light_spell_record
-from .dark_spellbook import dark_spell_record
+# from .dark_spellbook import dark_spell_record
+# Ne pas exposer dark_spell_record ici
+# pour eviter le circular import se declencher ici
+# --> crash du kaboom_0 aussi si non
 
 
 __all__ = ["light_spell_record", "dark_spell_record"]
--- mod06/ft_kaboom_1.py
+++ mod06/ft_kaboom_1.py
@@ -1,10 +1,9 @@
-from alchemy.grimoire.dark_spellbook import dark_spell_record
-
 if __name__ == "__main__":
     print("=== Kaboom 1 ===")
     print("Access to alchemy/grimoire/dark_spellbook.py directly")
     print("Test import now - THIS WILL RAISE AN UNCAUGHT EXCEPTION")
     print("Testing record dark spell: ", end="")
+    from alchemy.grimoire.dark_spellbook import dark_spell_record
     print((
         dark_spell_record('Fantasy', 'Earth, wind and fire')
         ))
